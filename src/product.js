2ed3cb69117e201d0d42187b1bf518cc297981b3
satra2806 <84612137+satra2806@users.noreply.github.com>
2021-10-21 22:27:10 +0530

    DudePlease

--- src/product.js
+++ src/product.js
@@ -11,7 +11,23 @@ router.get("/", async (req, res) => {
     res.json({
       status: 200,
       message: "Get data has successfully",
-    });
+    },    {
+        "id": 1,
+        "title": "Star girl"
+      },
+      {
+        "id": 2,
+        "title": "Five feet apart"
+      },
+      {
+        "id": 3,
+        "title": "Fifty shades of Grey"
+      },
+      {
+        "id": 4,
+        "title": "Fifty shades of Grey"
+      }
+    );
   } catch (error) {
     console.error(error);
     return res.status(500).send("Server error");
