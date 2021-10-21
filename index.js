a159bb47ba2daf7a84304cf8547473b91712170e
satra2806 <84612137+satra2806@users.noreply.github.com>
2021-10-21 22:25:10 +0530

    Nge

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT ||8080
+const product = require("./src/product");
+
+app.use(express.json({ extended: false }));
+
+app.use("/api/product", product);
+
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`)
+  });
